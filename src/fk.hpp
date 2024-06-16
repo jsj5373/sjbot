3d8134c254edc3dd3b5a004fe27f44947153f544
HDX <jsj5373@naver.com>
2024-06-17 08:56:43 +0900

    240614_update

---
+++ src/fk.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+
+#include <iostream>
+#include <eigen3/Eigen/Dense>
+
+#include <config.hpp>
+
+namespace TR_exca{
+    using Eigen::Vector3d;
+
+    class kinematics
+    {
+        private : 
+
+
+
+    };
+
+}
